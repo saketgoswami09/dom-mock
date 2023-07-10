064f3e3c1caff8f5aa945c201328050d8267be47
saketgoswami09 <110333651+saketgoswami09@users.noreply.github.com>
2023-07-10 14:42:09 +0530

    Add files via upload

---
+++ dom man/script.js
@@ -0,0 +1,7 @@
+const box = document.getElementById("box");
+
+box.addEventListener("click", () => {
+  box.style.backgroundColor = "yellow";
+  box.style.width = 600 ;
+  box.style.height = 600 ;
+});
